70ef455a15333bb86085d7735eddaa57cc131f53
Samuel Mbassa <mbassa@gmail.com>
2014-09-08 10:58:43 -0400

    Initial Commit

---
+++ Unit2Lesson3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Unit2Lesson3
+//
+//  Created by Samuel Mbassa on 9/8/14.
+//  Copyright (c) 2014 Samuel Mbassa. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
